03d27461149396230d7aa22d30fef0f8ab53503b
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-07-18 12:59:27 -0300

    Vídeo 2.2

--- src/main/java/br/com/alura/codechella/EventoController.java
+++ src/main/java/br/com/alura/codechella/EventoController.java
@@ -3,9 +3,11 @@ package br.com.alura.codechella;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/eventos")
@@ -14,8 +16,13 @@ public class EventoController {
     @Autowired
     private EventoService servico;
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping //(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<EventoDto> obterTodos() {
         return servico.obterTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<EventoDto> obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
     }
 }
--- src/main/java/br/com/alura/codechella/EventoService.java
+++ src/main/java/br/com/alura/codechella/EventoService.java
@@ -1,8 +1,11 @@
 package br.com.alura.codechella;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EventoService {
@@ -12,6 +15,12 @@ public class EventoService {
 
     public Flux<EventoDto> obterTodos() {
         return  repositorio.findAll()
+                .map(EventoDto::toDto);
+    }
+
+    public Mono<EventoDto> obterPorId(Long id) {
+        return  repositorio.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
                 .map(EventoDto::toDto);
     }
 }
