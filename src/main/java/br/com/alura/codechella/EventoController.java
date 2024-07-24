e663454a3c44d5ada8497cbb2825b321d4a12616
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-07-24 18:14:29 -0300

    Vídeo 5.1

--- src/main/java/br/com/alura/codechella/EventoController.java
+++ src/main/java/br/com/alura/codechella/EventoController.java
@@ -1,5 +1,6 @@
 package br.com.alura.codechella;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -50,6 +52,7 @@ public class EventoController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<EventoDto> cadastrar(@RequestBody EventoDto dto) {
         return servico.cadastrar(dto)
                 .doOnSuccess(e -> eventoSink.tryEmitNext(e));
--- src/test/java/br/com/alura/codechella/CodechellaApplicationTests.java
+++ src/test/java/br/com/alura/codechella/CodechellaApplicationTests.java
@@ -1,13 +1,37 @@
 package br.com.alura.codechella;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodechellaApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void cadastraNovoEvento() {
+		EventoDto dto = new EventoDto(null, TipoEvento.SHOW, "Kiss",
+				LocalDate.parse("2025-01-01"), "Show da melhor banda que existe");
+
+		webTestClient.post().uri("/eventos").bodyValue(dto)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(EventoDto.class)
+				.value(response -> {
+					assertNotNull(response.id());
+					assertEquals(dto.tipo(), response.tipo());
+					assertEquals(dto.nome(), response.nome());
+					assertEquals(dto.data(), response.data());
+					assertEquals(dto.descricao(), response.descricao());
+				});
 	}
 
 }
